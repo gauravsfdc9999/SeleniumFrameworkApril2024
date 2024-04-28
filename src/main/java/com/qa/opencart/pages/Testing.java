03ed17627e88bd457f887b811b0c0cfc27a3d9d7
gauravsfdc9999 <136590834+gauravsfdc9999@users.noreply.github.com>
2024-04-28 11:23:40 +0530

    Update and rename Testing to Testing.java

---
+++ src/main/java/com/qa/opencart/pages/Testing.java
@@ -0,0 +1,19 @@
+package com.qa.opencart.pages;
+
+import org.openqa.selenium.By;
+
+public class Testing {
+
+
+		
+		private final By nameLocator  = By.id("test");
+		
+		String i="Gaurav";
+		
+		public void getName() {
+			System.out.println("cart page value of i: "+i);
+			System.out.println("name");
+		}
+
+
+}
