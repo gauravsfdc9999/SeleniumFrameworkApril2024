59606ddbdc941e6d7a3d08da74f3f4dc28b9b040
gauravsfdc9999 <gauravsfdc.9988@outlook.com>
2024-04-28 10:27:34 +0530

    added cartpage

---
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -0,0 +1,13 @@
+package com.qa.opencart.pages;
+
+public class CartPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i=10;
+		System.out.println("cart page value of i: "+i);
+
+	}
+
+}
